54c00f9977d4517ddf1ee091546cc388257fba0c
PIM <pim@example.com>
2025-04-18 22:58:03 +0700

    Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react"
 import { Hero } from "../component/Hero";
 import { Scrolling } from "../component/Scrolling";
 import { About } from "../component/layout/About";
@@ -20,6 +21,7 @@ function App() {
   return (
     <>
       <FollowCursor />
+      <Analytics/>
 
       {/* BG */}
       <div className="fixed inset-0 -z-10 bg-neutral-950 bg-[radial-gradient(ellipse_80%_60%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
